750cb94a86afb21c1f02bcb1b8484121ea006260
hussein <118814567+husseinomar19@users.noreply.github.com>
2023-05-08 12:36:28 +0200

    header en main section add

--- index/index.php
+++ index/index.php
@@ -4,9 +4,23 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../CSS/home.css">
     <title>Getaways</title>
 </head>
 <body>
-    <h1>hallo</h1>
+    <header>
+      <nav>
+        <a id="logo" href="index.php">Getaways.nl</a>
+        <ul>
+            <li><a href="">Home</a></li>
+            <li><a href="">Over ons</a></li>
+            <li><a href="">Contact</a></li>
+        </ul>
+        <button id="inlog">Inloggen</button>
+      </nav>
+    </header>
+    <section class="main">
+        
+    </section>
 </body>
 </html>
