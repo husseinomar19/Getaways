7e5ebac00549c31d7f7c0ac05e8ee656c1a091aa
hussein <118814567+husseinomar19@users.noreply.github.com>
2023-05-08 13:00:44 +0200

    header maken en styelen.

--- index/index.php
+++ index/index.php
@@ -8,6 +8,20 @@
     <title>Getaways</title>
 </head>
 <body>
-    
+    <header>
+        <nav>
+            <a id="logo" href="home.php">Getaways.nl</a>
+            <ul>
+                <li><a href="home.php">Home</a></li>
+                <li><a href="over.php">Over Ons</a></li>
+                <li><a href="contact.php">Contact</a></li>
+            </ul>
+            <button id="inlog">Inloggen</button>
+        </nav>
+    </header>
+    <section class="main">
+
+    </section>
+   
 </body>
 </html>
