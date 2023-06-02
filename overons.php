251af4ee956a393608f7e7f69dc4d99ae5ec7a33
Sporrior <120579307+Sporrior@users.noreply.github.com>
2023-06-02 14:03:30 +0200

    1

---
+++ overons.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Over Ons</title>
+</head>
+    <header>
+        <nav>
+            <a id="logo" href="index.php"> <span id="logospan">G</span>eta<span id="logospan">W</span>ays.nl</a>
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="over.php">Over Ons</a></li>
+                <li><a href="contact.php">Contact</a></li>
+                <li><a href="#boeken">Boeken</a></li>
+            </ul>
+        </nav>
+    </header>
+<body>
+    
+</body>
+</html>
