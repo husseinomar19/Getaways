0e8b29fe4226fdc6bbc435273fee7a53f78e346a
Sporrior <120579307+Sporrior@users.noreply.github.com>
2023-06-02 14:12:15 +0200

    contact, overons .php

---
+++ index/contact.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../CSS/home.css">
+    <title>Over Ons</title>
+</head>
+    <header>
+        <nav>
+            <a id="logo" href="index.php"> <span id="logospan">G</span>eta<span id="logospan">W</span>ays.nl</a>
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="over.php">Over Ons</a></li>
+                <li><a href="contact.php">Contact</a></li>
+                <li><a href="#boeken">Boeken</a></li>
+            </ul>
+        </nav>
+    </header>
+<body>
+    
+</body>
+</html>
