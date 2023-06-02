f79ea9f6cfa9b06ea4c5cdfdadab43e494c03607
Sporrior <120579307+Sporrior@users.noreply.github.com>
2023-06-02 15:23:12 +0200

    opslaan wat ik vanaadg heb gedaan, na werk verder

--- index/overons.php
+++ index/overons.php
@@ -2,7 +2,14 @@
 <html lang="en">
 
 <head>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
+        integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
     <meta charset="UTF-8">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&family=Pacifico&display=swap"
+        rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../CSS/overons.css">
@@ -26,7 +33,19 @@
             <img src="../reizenimg/achtergrondoverons.jpeg" alt="">
         </div>
     </section>
-
+    <footer>
+        <ul>
+            <li><a href="">Voorwaarden</a></li>
+            <li><a href="">Cookies</a></li>
+            <li><a href="">Privacy policy</a></li>
+            <li><a href="">Sitemap</a></li>
+        </ul>
+        <ul>
+            <li><i class="fa-brands fa-facebook"></i></li>
+            <li><i class="fa-brands fa-instagram"></i></li>
+            <li><i class="fa-brands fa-twitter"></i></li>
+        </ul>
+    </footer>
 </body>
 
 </html>
