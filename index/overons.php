7c5b8d322d1999feb049606c121ca0baad7a921c
Sporrior <120579307+Sporrior@users.noreply.github.com>
2023-06-06 09:28:53 +0200

    foto

--- index/overons.php
+++ index/overons.php
@@ -1,24 +1,78 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
+        integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
     <meta charset="UTF-8">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&family=Pacifico&display=swap"
+        rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../CSS/overons.css">
     <link rel="stylesheet" href="../CSS/home.css">
+
     <title>Over Ons</title>
 </head>
-    <header>
-        <nav>
-            <a id="logo" href="index.php"> <span id="logospan">G</span>eta<span id="logospan">W</span>ays.nl</a>
-            <ul>
-                <li><a href="index.php">Home</a></li>
-                <li><a href="overons.php">Over Ons</a></li>
-                <li><a href="contact.php">Contact</a></li>
-                <li><a href="index.php#boeken">Boeken</a></li>
-            </ul>
-        </nav>
-    </header>
+<header>
+    <nav>
+        <a id="logo" href="index.php"> <span id="logospan">G</span>eta<span id="logospan">W</span>ays.nl</a>
+        <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="overons.php">Over Ons</a></li>
+            <li><a href="contact.php">Contact</a></li>
+            <li><a href="#boeken">Boeken</a></li>
+        </ul>
+    </nav>
+</header>
+
 <body>
-    
+    <section>
+        <div class="foto">
+            <img src="../reizenimg/tenerife.jpeg" alt="foto">
+        </div>
+        <div class="infotext">
+            test
+            Ut dictum nulla a lacinia molestie. Aliquam eleifend, leo nec dapibus volutpat, nisi leo tincidunt
+            tellus, sed cursus nunc enim vel orci. Integer ipsum sem, rhoncus eget bibendum non, consectetur in dui.
+            Quisque faucibus quis enim sit amet sodales. Pellentesque habitant morbi tristique senectus et netus et
+            malesuada fames ac turpis egestas. Suspendisse nec sollicitudin dolor. Maecenas nec malesuada mauris.
+            Donec est dui, ultrices tincidunt maximus sed, fermentum sed ipsum. Proin commodo tellus eu sodales
+            imperdiet. Ut purus felis, pellentesque in rutrum quis, tincidunt in quam. Praesent hendrerit eget
+            libero eu tincidunt. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos
+            himenaeos. Ut at nunc at sapien egestas dictum. Vivamus luctus sed urna sit amet porta. Interdum et
+            malesuada fames ac ante ipsum primis in faucibus. Proin ut elit ut mauris placerat porttitor non nec nunc.
+            Nunc egestas metus sed dignissim vehicula.
+            Quisque eleifend leo ac nulla placerat imperdiet. Quisque fermentum efficitur metus quis fermentum.
+            Mauris non eleifend mi, et eleifend odio. Donec vulputate, nibh in ultricies molestie, leo odio euismod
+            est, vitae lacinia quam dui vulputate enim. Pellentesque molestie, quam at tempus maximus, turpis lorem
+            elementum diam, non pellentesque sapien diam vel lectus. Nam ipsum eros, suscipit eu vehicula eu,
+            pellentesque sit amet sapien. Nam venenatis turpis at turpis
+            aliquam rutrum. Curabitur dignissim ligula ut consequat viverra. Aliquam sit amet tincidunt nisi. Morbi
+            mattis dignissim ligula nec dictum. Vivamus non tellus velit. Sed dapibus vulputate nulla vel convallis.
+            Nam metus nibh, elementum sit amet ligula sit amet, interdum varius sem. Fusce at tristique lorem, sit
+            amet vehicula mi. Cras sed ipsum vitae lorem pellentesque feugiat ullamcorper at turpis. Nam vulputate,
+            urna et suscipit eleifend, risus mauris lacinia eros, sed vestibulum velit dolor sed nulla. Fusce
+            rhoncus tortor nisi, nec faucibus nulla sodales id. Praesent mi turpis, tincidunt et enim non,
+            consectetur efficitur ex.
+        </div>
+    </section>
+    <footer>
+        <ul>
+            <li><a href="">Voorwaarden</a></li>
+            <li><a href="">Cookies</a></li>
+            <li><a href="">Privacy policy</a></li>
+            <li><a href="">Sitemap</a></li>
+        </ul>
+        <ul>
+            <li><i class="fa-brands fa-facebook"></i></li>
+            <li><i class="fa-brands fa-instagram"></i></li>
+            <li><i class="fa-brands fa-twitter"></i></li>
+        </ul>
+    </footer>
 </body>
+
 </html>
