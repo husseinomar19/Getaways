6f822892b31a1525136dca5cb5693abed03fce69
hussein <118814567+husseinomar19@users.noreply.github.com>
2023-06-20 12:18:07 +0200

    admin panel berichten php code toegevoegd

--- index/berichten.php
+++ index/berichten.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../CSS/berichten.css">
-    <title>aanpassen</title>
+    <title>Toevoegen</title>
 </head>
 <body>
     <div class="title">
@@ -14,28 +14,25 @@
     <div class="terug-admin">
         <a href="beheerder.php">Terug naar Beheerder panel</a>
     </div>
-    <?php
-// Connect to database
-$con = mysqli_connect("localhost", "root", "root", "vakantie");
+  <?php
+  include('config.php');
+  $stmt = $con->prepare(" SELECT * FROM ");
+  ?>    
+    <section class="sms">
+        <div class="briecht">
+            <h3>ddd</h3>
+        </div>
+        <div class="briecht">
+            <h3>dd</h3>
+        </div>
+        <div class="briecht">
+            <h3>ddd</h3>
+        </div>
+        <div class="briecht">
+            <a href="">Verwijderen</a>
+        </div>
 
-// Check connection
-if (mysqli_connect_errno()) {
-    echo "Failed to connect to MySQL: " . mysqli_connect_error();
-}
-
-// Query database
-$result = mysqli_query($con, "SELECT * FROM contact_form");
-
-// Display results
-while ($row = mysqli_fetch_array($result)) {
-    echo $row['name'] . "<br>";
-    echo $row['email'] . "<br>";
-    echo $row['message'] . "<br>";
-}
-
-// Close connection
-mysqli_close($con);
-?>
+    </section>
     
     
 </body>
